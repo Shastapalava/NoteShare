cd33b5846e755ebc597e317afbe9f1b11edc16c6
Rafael <r.ninalaya@outlook.com>
2018-10-27 22:07:14 -0400

    Implementation of views'

--- NoteShare/ordUser/models.py
+++ NoteShare/ordUser/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class ordUser(models.Model):
-	firstName = models.CharField()
-	lastName = models.CharField()
-	email = models.EmailField()
-	password = models.CharField() #do not leave password as is. Handle with hashing later on
+	firstName = models.CharField(max_length = 100)
+	lastName = models.CharField(max_length = 100)
+	email = models.EmailField(max_length = 100)
+	password = models.CharField(max_length = 100) #do not leave password as is. Handle with hashing later on
 
 class Note(models.Model):
 	creationDate = models.DateTimeField()
