1ebbf45c61605a53a0ccde49cfea0ea06e40406d
Anzhalika Shastapalava <angelika@Anzhalikas-MacBook-Air.local>
2018-12-11 14:12:36 -0500

    LockPost addition to post model

--- NoteShare/cms/admin.py
+++ NoteShare/cms/admin.py
@@ -7,7 +7,7 @@ from .forms import PostAdminForm
 
 @admin.register(Post) #this line does the same thing as admin.site.register(Post), but lets you also create your own ModelAdmin class to choose what you display in the admin interface
 class PostAdmin(CompareVersionAdmin):
-	list_display = ('locked','title', 'slug', 'author','publish','status') #this doesn't let you edit such things like the date created
+	list_display = ('title', 'slug', 'author','publish','status','locked') #this doesn't let you edit such things like the date created
 	list_filter = ('status', 'created', 'publish','author')
 	search_fields = ('title','body')
 	#prepopulated_fields = {'slug':('title',)}
