a3e0b228662552ada16d57bb625fde0b125f7189
Anastasiia Timashova <anastimashova@gmail.com>
2018-12-12 07:19:19 -0500

    added the user type to user creation and change forms and added test code that makes taboo words read only for non-superusers

--- NoteShare/cms/admin.py
+++ NoteShare/cms/admin.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 from .adminAna import *
 from .adminDavid import *
 from .adminAngelika import *
-=======
-from .adminAngelika import *
-from .adminDavid import *
-from .adminAna import *
->>>>>>> 4cc07b10bcba268f4c0cfe0e1c7f721eaeec6b85
